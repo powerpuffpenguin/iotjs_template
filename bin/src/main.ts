2d54ceb3fa76e3a1d7a0a83127fd449b9bde16f2
zuiwuchang <zuiwuchang@gmail.com>
2023-06-13 17:11:59 +0800

    tsc

---
+++ bin/src/main.ts
@@ -0,0 +1,4 @@
+import { os, arch, version } from "iotjs";
+console.log("os:", os)
+console.log("arch:", arch)
+console.log("version:", version)
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+module.exports = {
+    target: ['node', 'es5'],
+    mode: 'production',
+    entry: {
+        main: './bin/src/main.ts',
+    },
+    output: {
+        path: path.resolve(__dirname, 'bin'),
+        filename: "bundle.js",
+        libraryTarget: "commonjs",
+    },
+    module: {
+        rules: [
+            { test: /\.ts$/, use: 'ts-loader' }
+        ],
+    },
+    externals: [
+        "iotjs",
+        /^iotjs\//,
+    ],
+    resolve: {
+        modules: [
+            'node_modules/'
+        ],
+        descriptionFiles: ['package.json'],
+        extensions: ['.ts'],
+    },
+}
